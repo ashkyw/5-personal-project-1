d2450fa416f332f470a3f28a3668be2713fbf359
ashkyw <ashkyw@gmail.com>
2026-07-04 14:52:37 -0500

    added interactive window

--- qr-creator/src/gui.py
+++ qr-creator/src/gui.py
@@ -2,11 +2,32 @@ import tkinter as tk
 from tkinter import ttk
 from tkinterdnd2 import DND_FILES, TkinterDnD
 
-def draw_window()-> None:
+def draw_simple_window()-> None:
     # Draw user window
     root = tk.Tk()
     frm = ttk.Frame(root, padding=10)
     frm.grid()
-    tk.Label(frm, text="Hello, World!").grid(column=0, row=0)
-    tk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+    tk.Label(frm, text="Hello, World!").grid(column=0, row=2)
+    tk.Button(frm, text="Select File")
+    tk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=0)
     root.mainloop()
+
+def draw_interactive_window()-> None:
+# create special TkinterDnD Window
+    root = TkinterDnD.Tk()
+    root.geometry("500x300")
+    root.title("File Drag and Drop")
+
+    label = tk.Label(root, text="Drag & Drop a file here", bg="lightgray", width=50, height=10)
+    label.pack(pady=50)
+
+# enable drop events
+    label.drop_target_register(DND_FILES)
+    label.dnd_bind("<<Drop>>", drop_file)
+
+    root.mainloop()
+
+def drop_file(event):
+    # Handles the dropped file path
+    file_path = event.data
+    label.config(text=f"Dropped file:\n{file_path}")
--- qr-creator/src/qr_creator.py
+++ qr-creator/src/qr_creator.py
@@ -1,12 +1,13 @@
 import qrcode
 import openpyxl
-from gui import draw_window
+from gui import draw_simple_window, draw_interactive_window, drop_file
 
 def generate_qr_codes(file: str)-> str:
     PASS
 
 def main()-> None:
-    draw_window()
+    draw_interactive_window()
+    drop_file()
 
 if __name__ == "__main__":
     main()
