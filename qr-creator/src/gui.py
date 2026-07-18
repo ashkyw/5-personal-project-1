fb29004cc4b5321925cf316983363d108a864508
ashkyw <ashkyw@gmail.com>
2026-07-18 17:00:15 -0500

    added in-file QRCode creation

--- qr-creator/src/gui.py
+++ qr-creator/src/gui.py
@@ -1,14 +1,18 @@
 import qrcode
 import openpyxl
 import customtkinter as ctk
+from io import BytesIO
 from tkinterdnd2 import DND_FILES, TkinterDnD
+from openpyxl.drawing.image import Image
 
 class TopLevelWindow(ctk.CTkToplevel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, master, text):
+        super().__init__(master)
         self.geometry("400x300")
 
-        self.label = ctk.CTkLabel(self, text="TopLevelWindow")
+        self.text: str = text
+
+        self.label = ctk.CTkLabel(self, text=text)
         self.label.pack(padx=20, pady=20)
 
 class LabelText(ctk.CTkFrame):
@@ -29,7 +33,7 @@ class DragAndDropFrame(ctk.CTkFrame):
         self.on_files: list[str] = on_files
         self.valid_extensions: tuple[str] = valid_extensions
 
-        self.grid_columnconfigure(0, weight=10)
+        self.grid_columnconfigure(0, weight=1)
 
         self.title = ctk.CTkLabel(self, text=self.title, font=ctk.CTkFont(size=16, weight="bold"))
         self.title.grid(row=0, column=0, padx=10, pady=(10,0), sticky="ew")
@@ -122,34 +126,33 @@ class App(TkinterDnD.Tk):
     def __init__(self):
         super().__init__()
         # Set App window settings
-        self.title("App title goes here")
+        self.title("Excel File Formatter")
         self.geometry("800x640")
         self.grid_columnconfigure((0, 1), weight=1)
         self.grid_rowconfigure(0, weight=1)
-        self.configure(bg="gray30")
+        self.configure(bg="gray10")
 
         self.files: list[str] = []
 
         self.toplevel_window = None
 
-        self.button = ctk.CTkButton(self, text="New window", command=self.create_toplevel_window)
-        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
-
-        # Set ButtonFrame buttons & settings
-        self.button_frame = ButtonFrame(
-            self, "Functions", text=["New Window", "Print Success"],
-            commands=[self.create_toplevel_window, self.test_adding_commands_to_buttons]
-        )
-        self.button_frame.grid(row=0, column=0, padx=10, pady=(10,0), sticky="nsew")
-
         # Set DragAndDropFrame labels & settings
         self.drop_frame = DragAndDropFrame(
             self, "Drag files here...", on_files=self.files_dropped,
-            # uncomment for testing purposes valid_extensions=(".pdf", ".png")
-            valid_extensions=(".xlsx", ".xlsm", ".csv", ".xlsb")
+            valid_extensions=(".xlsx", ".xlsm", ".xlsb")
         )
-        self.drop_frame.grid(row=0, column=1, padx=(0,10), pady=(10,0), sticky="nsew", columnspan=2)
-        self.drop_frame.configure(fg_color="transparent")
+        self.drop_frame.grid(row=0, column=1, padx=(0,10), pady=(10,0), sticky="nsew")
+        self.drop_frame.configure(fg_color="gray30", border_width=1, border_color="black")
+
+        # Set ButtonFrame buttons & settings
+        self.button_frame = ButtonFrame(
+            self, "Functions", text=["Create QR Codes"],
+            commands=[self.create_qrcodes]
+        )
+        self.button_frame.grid(row=0, column=0, padx=10, pady=(10,0), sticky="nsew")
+
+        self.button = ctk.CTkButton(self, text="Clear List", command=self.clear_files_list)
+        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
 
     # Hands files off to other functions
     def files_dropped(self, files: list[str]) -> None:
@@ -159,68 +162,67 @@ class App(TkinterDnD.Tk):
         # If frame exists, clear it
         if hasattr(self, "drop_text_frame"):
             self.drop_text_frame.destroy()
-            self.remove_frame.destroy()
 
         # Set child labels & values
         self.drop_text_frame = LabelText(self.drop_frame, self.files)
         self.drop_text_frame.grid(row=1, column=0, padx=10, pady=(10,0), sticky="ew")
 
-        #self.create_remove_frame(files)
-
-    # Creates button frame to remove file from files list
-    def create_remove_frame(self, files: list[str]) -> None:
-        # If you go with this function, you need to update remove_file_from_list to pop items from the list, then update the buttons with the new list indices.
-        # Really, it's a lot of work for an algorithm that personal developmental-wise would be fantastic, but in reality is unnecessary when users can just re-drag the files into the window to reset
-        self.remove_frame = ButtonFrame(self.drop_frame, "Remove Files")
-        self.remove_frame.grid(row=1, column=1, padx=10, pady=(10,0), sticky="ew")
-        if self.files != None:
-            for i, file in enumerate(self.files):
-                self.button = ctk.CTkButton(self.remove_frame, text="Remove", command=self.test_adding_commands_to_buttons)
-                self.button.grid(row=i+1, column=1, padx=10, pady=(10,0), sticky="ew")
-
     # Button functions
-    def create_toplevel_window(self) -> None:
+    def create_toplevel_window(self, text) -> None:
 
         if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-            self.toplevel_window = TopLevelWindow(self)
+            self.toplevel_window = TopLevelWindow(self, text=text)
         else:
             self.toplevel_window.focus()
 
+    # Clear current active list
+    def clear_files_list(self) -> None:
+        self.files = []
+
+        if hasattr(self, "drop_text_frame"):
+            self.drop_text_frame.destroy()
+
+    # Fun button to test things
     def test_adding_commands_to_buttons(self) -> str:
         print("Every thing works")
 
-    def remove_file_from_list(self, files: list [str]) -> None:
-        # See note in create_remove_frame for more details
-        #self.files.pop()
-        PASS
+    # Creates QR Codes and stores them in excel file
+    def create_qrcodes(self) -> None:
+        if self.files == []:
+            self.create_toplevel_window("No files found")
+        else:
+            for file in self.files:
+                # set workbook and sheet
+                workbook = openpyxl.load_workbook(file)
+                ws = workbook.active
+                file_path, extension = file.split(".")
+                for i, row in enumerate(ws.iter_rows(values_only=True), start=1):
+                    if row != (None,):
+                        self.format_excel_sheet(ws, i)
+                        cleaned_row = "".join(map(str, row))
+                        qr_img = qrcode.make(cleaned_row)
+                        qr_img = self.format_qrcode_images(qr_img, i)
+                        #ws.add_image(qr_img, f"C{i}")
+                        ws.add_image(qr_img, f"C{i}")
+                workbook.save(f"{file_path}_qrcodes.{extension}")
+                self.create_toplevel_window("Operation Success!")
 
-    def create_qrcodes(files: list[str]) -> None:
-        if files == []:
-            # Setup some sort of error system. Easiest is likely a new top level window saying the list is empty
-            PASS
-        # Below will work for single files, but need to come up with a way to successfully iterate over multiple files at once
-        # to ensure all files are correctly handled.
-        for file in files:
-            # set active workbook
-            workbook = openpyxl.load_workbook(file)
-            # set active worksheet
-            ws = workbook.active
-            for i, row in enumerate(ws.iter_rows(values_only=True)):
-                if row != (None,):
-                    cleaned_row = "".join(map(str, row))
-                    img = qrcode.make(cleaned_row)
-                    img_column = ws.cell(row=i+1, column=2)
-                    img_column.value = img
-                    #img.save(f"{i}.png")
+    # Helper function to format Excel sheet
+    def format_excel_sheet(self, ws, row: int) -> None:
+        ws.row_dimensions[row].height = 185
+        ws.column_dimensions["B"].width = 2
+        ws.column_dimensions["C"].width = 28
 
-        '''
-        for row in ws.iter_rows(values_only=True):
-            if row != (None,):
-                cleaned_row = "".join(map(str, row))
-                img = qrcode.make(cleaned_row)
-                img.save(f"{i}.png")
-                i += 1
-        '''
+    # Helper function to format and temp store QR Codes
+    def format_qrcode_images(self, img, num: int) -> Image:
+        buffer = BytesIO()
+        img.save(buffer, format="PNG")
+        buffer.seek(0)
+        img = Image(buffer)
+        img.width = 225
+        img.height = 225
+        img.anchor = f"C{num}"
+        return img
 
     def button_callback(self) -> None:
         print("checked checkboxes", self.checkbox_frame.get())
